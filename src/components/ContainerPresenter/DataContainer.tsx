6c00cd33aecdde0e37984f55c22074a1c11ca4c4
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-03-04 23:51:15 +0000

    add container presenter

--- src/components/ContainerPresenter/DataContainer.tsx
+++ src/components/ContainerPresenter/DataContainer.tsx
@@ -1,6 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { DataPresenter } from "./DataPresenter"
+
+type DataItem = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
+
 export const DataContainer = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data/data.json");
+        if (!response.ok) {
+          throw new Error("Error al cargar los datos");
+        }
+        const result = await response.json()
+        setData(result)
+      } catch (error) {
+        setError(`Error al cargar los datos: ${error}`)
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <div>
+      {loading && <p>Cargando...</p>}
+      {error && <p>{error}</p>}
+      <DataPresenter data={data} />
     </div>
   );
 };
--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,33 @@
-const UserComponent = () => {
+import { useState } from "react";
+
+function useLoading(initialLoading: boolean = false) {
+  const [isLoading, setIsLoading] = useState(initialLoading);
+
+  return {
+    isLoading,
+    setIsLoading
+  }
+}
+
+type UserProps = {
+  name: string;
+}
+
+const UserComponent: React.FC<UserProps> = ({ name }) => {
+  const { isLoading, setIsLoading} = useLoading(false);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  
   return (
     <div>
+      <p>Hello, {name} !</p>
+      <button onClick={() => setIsLoading(true)}>Simulate Loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name="Teffcode" />;
 };
