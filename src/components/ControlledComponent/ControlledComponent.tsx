d6b91bb816758dd50fae137616df85b0af66a28b
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-03-06 00:10:22 +0000

    add controlled and uncontrolled component in react

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,17 @@
+import React, { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("");
+
   return (
     <div>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Ingresa el código del cupón. Ej: 10OFF"
+      />
+      <p>Cupón de descuento: <b>{value}</b></p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,22 @@
+import React, { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`🛒 Nuevo producto en el carrito: ${inputRef.current.value} ✅`)
+    }
+  }
+
   return (
     <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Nombre del producto. Ej: Manzana"
+      />
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
